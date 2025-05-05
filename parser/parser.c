287b87db947813d4f2218ee2f0bd897d02aa0d7c
thehrik <hrik05malakar@gmail.com>
2025-05-05 10:17:52 +0530

    checking if the number is too large in the parser

--- parser/parser.c
+++ parser/parser.c
@@ -1,15 +1,11 @@
-#include "parser.h"
-
-#include "../result/result.h" // or "parser.h" if it includes result.h
-#include <stddef.h>           // for NULL
+#include <limits.h>
+#include <stddef.h>
+#include "../result/result.h"
 
 ResultInt parse_to_int(const char *str)
 {
-
   if (str == NULL || *str == '\0')
-  {
     return (ResultInt){RESULT_ERR, .data.err = "Input string is empty"};
-  }
 
   int sign = 1;
   if (*str == '-')
@@ -19,22 +15,31 @@ ResultInt parse_to_int(const char *str)
   }
 
   if (*str < '0' || *str > '9')
-  {
     return (ResultInt){RESULT_ERR, .data.err = "No digits found after sign"};
-  }
 
   int num = 0;
   while (*str >= '0' && *str <= '9')
   {
-    num = num * 10 + (*str - '0');
+    int digit = *str - '0';
+
+    if (sign == 1)
+    {
+      if (num > (INT_MAX - digit) / 10)
+        return (ResultInt){RESULT_ERR, .data.err = "Number too large"};
+    }
+    else
+    {
+      // For negative numbers, check INT_MIN
+      if (num < (INT_MIN + digit) / 10)
+        return (ResultInt){RESULT_ERR, .data.err = "Number too small"};
+    }
+
+    num = num * 10 + sign * digit;
     str++;
   }
 
-  // If there are any non-digit characters left, it's an error
   if (*str != '\0')
-  {
     return (ResultInt){RESULT_ERR, .data.err = "Invalid character in input string"};
-  }
 
-  return (ResultInt){RESULT_OK, .data.ok = sign * num};
+  return (ResultInt){RESULT_OK, .data.ok = num};
 }
